bc546f6daaf7d2ac8c58d51ffb6403137e5d19ec
Vincent Clyde Burgos <clydeburgos@gmail.com>
2024-06-22 12:06:54 +0800

    removed unnecessary comments small clean up

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ContactManagement.Data;
 using ContactManagement.Models;
@@ -19,13 +14,12 @@ namespace ContactManagement.Controllers
             _context = context;
         }
 
-        // GET: Contacts
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Contacts.ToListAsync());
+            var data = await _context.Contacts.ToListAsync();
+            return View(data);
         }
 
-        // GET: Contacts/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -42,16 +36,11 @@ namespace ContactManagement.Controllers
 
             return View(contact);
         }
-
-        // GET: Contacts/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Contacts/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Email,PhoneNumber,Address")] Contact contact)
@@ -65,7 +54,6 @@ namespace ContactManagement.Controllers
             return View(contact);
         }
 
-        // GET: Contacts/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -81,9 +69,6 @@ namespace ContactManagement.Controllers
             return View(contact);
         }
 
-        // POST: Contacts/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email,PhoneNumber,Address")] Contact contact)
@@ -116,7 +101,6 @@ namespace ContactManagement.Controllers
             return View(contact);
         }
 
-        // GET: Contacts/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -134,7 +118,6 @@ namespace ContactManagement.Controllers
             return View(contact);
         }
 
-        // POST: Contacts/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
