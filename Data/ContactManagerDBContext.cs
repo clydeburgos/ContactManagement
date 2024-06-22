daa3718cc82be41aa39d761b4e80af2c82cd441b
Vincent Clyde Burgos <clydeburgos@gmail.com>
2024-06-22 11:17:07 +0800

    Add project files.

---
+++ Data/ContactManagerDBContext.cs
@@ -0,0 +1,26 @@
+﻿using ContactManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManagement.Data
+{
+    public class ContactManagerDBContext : DbContext
+    {
+        public ContactManagerDBContext(DbContextOptions<ContactManagerDBContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>().HasData(
+                new Contact { 
+                    Id = 1, 
+                    FirstName = "John", 
+                    LastName = "Doe", 
+                    Email = "john.doe@example.com", 
+                    PhoneNumber = "123-456-7890", 
+                    Address = "123 Main St" 
+                }
+            );
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ Migrations/20240622031158_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "Address", "Email", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[] { 1, "123 Main St", "john.doe@example.com", "John", "Doe", "123-456-7890" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
